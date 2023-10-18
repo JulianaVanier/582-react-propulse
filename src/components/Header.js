5df4f7619535b4279658a7e3fd78b7bac17b0f46
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-10-18 11:55:35 -0400

    created header component

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image';
+
+function NavScrollExample() {
+  return (
+    <Navbar expand="lg" className="nav-bar" bg="dark" data-bs-theme="dark">
+      <Container >
+        <Navbar.Brand href="#"><Image src="/assets/image/propulse-sportswear-high-resolution-logo-black-on-transparent-background.png" /></Navbar.Brand>
+        
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action5">
+                Something else here
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
+          </Nav>
+          <Form className="d-flex align-items-center">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="form-control-sm me-2"
+              aria-label="Search"
+            />
+            <Button className="btn-search" variant="outline-light">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
