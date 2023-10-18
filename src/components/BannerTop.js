e302269dc674db13c70528d3f2da3a162b4442b4
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-10-18 11:55:50 -0400

    created bannertop component

---
+++ src/components/BannerTop.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function BannerTop() {
+  return (
+    <>
+            <section class="banner_top" role="banner">
+            <div class="text">
+                <h1>unbeatable offers</h1>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing
+                    elit. Esse ipsa accusamus quaerat velit aliquid ad
+                    perferendis! Explicabo minus consectetur repudiandae.
+                </p>
+                <a class="btn" href="">Learn More</a>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default BannerTop
