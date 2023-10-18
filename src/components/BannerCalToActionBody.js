8b534195be49cbfd30cc4b778f83bd5c8a1744e4
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-10-18 17:24:29 -0400

    created Banner Call to Action

---
+++ src/components/BannerCalToActionBody.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function BannerCalToActionBody() {
+  return (
+    <>
+      <section class="banner_small">
+        <div class="box_banner_small">
+          <h5>Complete your winter look</h5>
+          <a class="btn" href="">Shop now</a>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default BannerCalToActionBody;
