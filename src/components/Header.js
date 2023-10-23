6518a838d9293dff1e609d07bb8b7481e4185acf
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-10-23 18:23:39 -0400

    added href to logo in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,54 @@
-import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Image from 'react-bootstrap/Image';
-
-
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Image from "react-bootstrap/Image";
 
 function Header() {
-
   function goContact() {
     window.location.href = "/contact";
     // navigate(-1);
-}
+  }
+
+  function goHome() {
+    window.location.href = "/";
+    // navigate(-1);
+  }
   return (
     <Navbar expand="lg" className="nav-bar" bg="dark" data-bs-theme="dark">
-      <Container >
-        <Navbar.Brand href="#"><Image src="/assets/image/propulse-sportswear-high-resolution-logo-black-on-transparent-background.png" /></Navbar.Brand>
-        
+      <Container>
+        <Navbar.Brand href="#">
+          <Image
+            onClick={() => {
+              goHome();
+            }}
+            src="/assets/image/propulse-sportswear-high-resolution-logo-black-on-transparent-background.png"
+          />
+        </Navbar.Brand>
+
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
             <Nav.Link href="#action1">All Sports</Nav.Link>
-            <Nav.Link href="#action2" onClick={ () => {goContact()}}>Contact</Nav.Link>
+            <Nav.Link
+              href="#action2"
+              onClick={() => {
+                goContact();
+              }}
+            >
+              Contact
+            </Nav.Link>
             <NavDropdown title="Kids" id="navbarScrollingDropdown">
               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                New Arrivals
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action4">New Arrivals</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Footwear
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action5">Footwear</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link href="#" disabled>
               Men
@@ -49,7 +61,9 @@ function Header() {
               className="form-control-sm me-2"
               aria-label="Search"
             />
-            <Button className="btn-search" variant="outline-light">Search</Button>
+            <Button className="btn-search" variant="outline-light">
+              Search
+            </Button>
           </Form>
         </Navbar.Collapse>
       </Container>
